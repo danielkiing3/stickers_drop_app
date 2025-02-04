06fd0ee43dff5cf44c07292b2f0ed349ff07bdd9
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-02-05 00:49:02 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/stickers_drop_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stickers_drop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(systemNavigationBarColor: Colors.transparent),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
