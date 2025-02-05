dd274c55bf2e07166f3e2aa0c7b3d457100ee5a8
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-02-05 03:05:02 +0100

    Start implementation for Drop Region

---
+++ lib/drop_region.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:super_drag_and_drop/super_drag_and_drop.dart';
+
+// class CustomDropRegion extends StatelessWidget {
+//   const CustomDropRegion({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DropRegion(
+//       formats: const <DataFormat<Object>>[
+//         Formats.jpeg,
+//         Formats.png,
+//         Formats.heif,
+//       ],
+//       onDropOver: (p0) {},
+//       onPerformDrop: (p0) {},
+//       child: Placeholder(),
+//     );
+//   }
+// }
