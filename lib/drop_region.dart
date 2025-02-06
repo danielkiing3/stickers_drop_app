12e92de4c79a1e0674ef97432c03ba96ffebd9dc
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-02-06 11:31:26 +0100

    Adding onDropEnter and leave functionality to Custom Drop Region

--- lib/drop_region.dart
+++ lib/drop_region.dart
@@ -1,30 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 
-// class CustomDropRegion extends StatefulWidget {
-//   const CustomDropRegion({
-//     super.key,
-//     required this.child,
-//   });
+class CustomDropRegion extends StatefulWidget {
+  const CustomDropRegion({
+    super.key,
+    required this.child,
+    required this.onDropEnter,
+    required this.onDropLeave,
+  });
 
-//   final Widget child;
+  final Widget child;
+  final VoidCallback onDropEnter;
+  final VoidCallback onDropLeave;
 
-//   @override
-//   State<CustomDropRegion> createState() => _CustomDropRegionState();
-// }
+  @override
+  State<CustomDropRegion> createState() => _CustomDropRegionState();
+}
 
-// class _CustomDropRegionState extends State<CustomDropRegion> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return DropRegion(
-//       formats: const <DataFormat<Object>>[
-//         Formats.jpeg,
-//         Formats.png,
-//         Formats.heif,
-//       ],
-//       onDropOver: (p0) {},
-//       onPerformDrop: (p0) {},
-//       child: widget.child,
-//     );
-//   }
-// }
+class _CustomDropRegionState extends State<CustomDropRegion> {
+  @override
+  Widget build(BuildContext context) {
+    return DropRegion(
+      formats: const <DataFormat<Object>>[
+        Formats.jpeg,
+        Formats.png,
+        Formats.heif,
+      ],
+      onDropOver: (p0) {
+        return DropOperation.copy;
+      },
+      onPerformDrop: (PerformDropEvent event) async {
+        // Getting the dropped data
+        final item = event.session.items.first;
+
+        // Getting the data reader
+        final reader = item.dataReader!;
+
+        if (reader.canProvide(Formats.png)) {
+          reader.getFile(
+            Formats.png,
+            (file) async {
+              final data = await file.readAll();
+
+              //TODO: Store the data somewhere
+            },
+            onError: (error) {
+              print('Error reading value $error');
+            },
+          );
+        } else {}
+      },
+      onDropEnter: (_) {
+        widget.onDropEnter();
+      },
+      onDropLeave: (_) {
+        widget.onDropLeave();
+      },
+      child: widget.child,
+    );
+  }
+}
