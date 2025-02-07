e588baf7fc699a2573de17156848d98fdb5cee40
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-02-07 02:13:18 +0100

    Added swipe screen

---
+++ lib/presentation/screens/swipe_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:stickers_drop_app/presentation/widgets/animated_sticker_transform.dart';
+import 'package:stickers_drop_app/data/image_source.dart';
+import 'package:stickers_drop_app/presentation/widgets/image_with_border_and_shadow.dart';
+
+class SwipeScreen extends StatefulWidget {
+  const SwipeScreen({super.key, required this.topImage});
+
+  final ImageProvider topImage;
+
+  @override
+  State<SwipeScreen> createState() => _SwipeScreenState();
+}
+
+class _SwipeScreenState extends State<SwipeScreen> {
+  int selectedIndex = 0;
+  double dragOffset = 0.0;
+  double totalDrag = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 253, 242, 223),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Spacer(),
+            GestureDetector(
+              onHorizontalDragStart: (_) {
+                totalDrag = 0.0;
+              },
+              onHorizontalDragUpdate: (details) {
+                totalDrag += details.delta.dx;
+              },
+              onHorizontalDragEnd: (_) {
+                if (totalDrag > 30) {
+                  if (selectedIndex > 0) {
+                    selectedIndex--;
+                  }
+                } else if (totalDrag < -30) {
+                  if (selectedIndex < ImageSource.asset.length) {
+                    selectedIndex++;
+                  }
+                }
+
+                setState(() {});
+              },
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  for (final (index, asset)
+                      in ImageSource.asset.reversed.indexed)
+                    Builder(builder: (context) {
+                      final newIndex = ImageSource.asset.length - index;
+                      final isSelected = newIndex < selectedIndex;
+
+                      final double scale = isSelected
+                          ? 1
+                          : 1 - ((newIndex - selectedIndex) * 0.02);
+
+                      final double rotation =
+                          newIndex == selectedIndex ? 0 : 0.2;
+
+                      final offset = isSelected
+                          ? const Offset(-350, 0)
+                          : Offset(
+                              ((newIndex - selectedIndex) * 10),
+                              ((newIndex - selectedIndex) * -5),
+                            );
+
+                      return AnimatedStickerTransform(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        scale: scale,
+                        offset: offset,
+                        rotation: rotation,
+                        child: SizedBox(
+                          width: 200,
+                          child: ImageWithBorderAndShadow(image: asset),
+                        ),
+                      );
+                    }),
+                  Builder(
+                    builder: (context) {
+                      final offset = selectedIndex > 0
+                          ? const Offset(-350, 0)
+                          : const Offset(0, 0);
+
+                      return AnimatedStickerTransform(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        scale: 1,
+                        rotation: 0,
+                        offset: offset,
+                        child: SizedBox(
+                          width: 200,
+                          child: ImageWithBorderAndShadow(
+                              imageProvider: widget.topImage),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back_ios_new_rounded, size: 40),
+                ),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.share, size: 40)),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.delete, size: 40)),
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
