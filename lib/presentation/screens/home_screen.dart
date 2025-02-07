fedd8f68a6e13c9bb4a7104e3ec63948df69d3b5
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-02-07 02:41:58 +0100

    Added comment

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,5 @@
-import 'dart:typed_data';
-
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:stickers_drop_app/presentation/widgets/drop_region.dart';
-import 'package:stickers_drop_app/data/image_source.dart';
-import 'package:stickers_drop_app/presentation/screens/swipe_screen.dart';
-import 'package:stickers_drop_app/utils/cached_image_provider.dart';
-import 'package:stickers_drop_app/presentation/widgets/image_with_border_and_shadow.dart';
+import 'package:stickers_drop_app/presentation/widgets/home_grid_content.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,6 +14,7 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              // -- Header
               Row(
                 children: [
                   Text(
@@ -48,7 +42,11 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
+
+              // -- Grid Content
               Flexible(child: HomeGridContent()),
+
+              // -- Bottom Section
               SizedBox(height: 24),
               Text(
                 '? HOW TO USE',
@@ -58,102 +56,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class HomeGridContent extends StatefulWidget {
-  const HomeGridContent({super.key});
-
-  @override
-  State<HomeGridContent> createState() => _HomeGridContentState();
-}
-
-class _HomeGridContentState extends State<HomeGridContent> {
-  final ValueNotifier<int?> hoverIndex = ValueNotifier<int?>(null);
-  final ValueNotifier<CacheMemoryImageProvider?> dropImage =
-      ValueNotifier<CacheMemoryImageProvider?>(null);
-
-  void updateHoverIndex(int index) {
-    hoverIndex.value = index;
-  }
-
-  void resetHoverIndex() {
-    hoverIndex.value = null;
-  }
-
-  void storeImage(Uint8List file) {
-    dropImage.value = CacheMemoryImageProvider(
-      'app://dropImage${DateTime.now()}',
-      file,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder<int?>(
-      valueListenable: hoverIndex,
-      builder: (context, hover, _) {
-        return GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            mainAxisSpacing: 12,
-            crossAxisSpacing: 12,
-            crossAxisCount: 3,
-          ),
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: 9,
-          itemBuilder: (context, index) {
-            return CustomDropRegion(
-              onDropEnter: () => updateHoverIndex(index),
-              onDropLeave: resetHoverIndex,
-              onStoreImage: storeImage,
-              child: DottedBorder(
-                borderType: BorderType.RRect,
-                radius: const Radius.circular(12),
-                dashPattern: const [10, 10],
-                color: hover == index
-                    ? const Color.fromARGB(255, 47, 24, 243)
-                    : Colors.black.withOpacity(0.2),
-                strokeWidth: 2,
-                child: Center(
-                  child: index != 0
-                      ? Transform.scale(
-                          scale: 0.8,
-                          child: ImageWithBorderAndShadow(
-                            image: ImageSource.asset[index - 1],
-                          ),
-                        )
-                      : ValueListenableBuilder<CacheMemoryImageProvider?>(
-                          valueListenable: dropImage,
-                          builder: (context, image, _) {
-                            return image != null
-                                ? GestureDetector(
-                                    onTap: () {
-                                      Navigator.of(context)
-                                          .push(MaterialPageRoute(
-                                        builder: (context) => SwipeScreen(
-                                          topImage: image,
-                                        ),
-                                      ));
-                                    },
-                                    child: ImageWithBorderAndShadow(
-                                      imageProvider: image,
-                                    ),
-                                  )
-                                : const Text(
-                                    'DRAG AND DROP YOUR STICKER',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(color: Colors.grey),
-                                  );
-                          },
-                        ),
-                ),
-              ),
-            );
-          },
-        );
-      },
     );
   }
 }
